17c18f79771dfbee3d7e7cc9e81638f4c30c4ed7
abcdlsj <lisongjianshuai@gmail.com>
2020-10-18 17:54:37 +0800

    update

--- default.cpp
+++ default.cpp
@@ -4,14 +4,5 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
-    // code:
-
-    fclose(stdin);
-    fclose(stdout);
+    
 }
