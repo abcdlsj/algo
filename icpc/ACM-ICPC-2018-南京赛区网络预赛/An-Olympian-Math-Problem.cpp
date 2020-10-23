b69df07cf900fb821a6e9f0f20b05237d503780a
abcdlsj <lisongjianshuai@gmail.com>
2020-10-23 11:15:02 +0800

    update in windows

---
+++ icpc/ACM-ICPC-2018-南京赛区网络预赛/An-Olympian-Math-Problem.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+    ll n; cin >> n;
+    cout << n - 1 << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    // 
+    int T; cin >> T;
+    while (T--) {
+        solve();
+    }
+}
